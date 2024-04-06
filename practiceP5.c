930f62d0372d710c54ce2b35690e8574a28a5464
mohsin6862 <mohsinctg2206@gmail.com>
2024-04-07 02:58:27 +0600

    prctice problem 5 solution added

---
+++ practiceP5.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+        int min =a;
+    if(a<b && a<c){
+        min = a;
+        printf("%d ",min);
+    }
+    else if(b<a && b<c){
+        min =b;
+        printf("%d ",min);
+    }
+    else{
+        min =c;
+        printf("%d ",min);
+    }
+    int max = a;
+    if(a>b && a>c){
+        max =a;
+        printf("%d ",max);
+    }
+    else if(b>a && b>c){
+        max=b;
+        printf("%d ",max);
+    }
+    else{
+        max=c;
+        printf("%d ",max);
+    }
+
+    return 0;
+}
