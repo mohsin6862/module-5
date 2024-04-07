493685b10fef2967972e8650b779f825c628ed79
mohsin6862 <mohsinctg2206@gmail.com>
2024-04-07 06:19:41 +0600

    New solution added

---
+++ problemP.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    int digit =(a/1000);
+        if(digit%2==0){
+            printf("EVEN");
+        }
+        else{
+            printf("ODD");
+        }
+    
+}
